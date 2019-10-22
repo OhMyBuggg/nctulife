26d9e2620fd482c24828fa79cbbde32ebf5271a1
OhMyBug <sa0963278610@gmail.com>
2019-10-23 01:10:04 +0800

    build all page

---
+++ page-templates-en/template-en-contact.php
@@ -0,0 +1,328 @@
+<?php
+/*
+ * Template Name: en-contact
+ */
+?>
+
+<?php get_header('en_header'); ?>
+
+<div class="page-intro">
+    <div class="container intro-mobile-container">
+        <div class="intro-bigTitle_1">聯絡我們 英文版</div>
+        <!--<div class="map"><a href="https://goo.gl/maps/qo5CweUFBWG2">地理位置(On Google Map)</a></div>-->
+        <div class="ra-circle_first">
+            <div class="page-1">
+            <div class="nextpage"></div>
+            <div class="ra-circle_first_block_1">
+                <div class="ra-circle_1"></div>
+                <div class="first_block_context_block">
+                    <div class="first_first_line">行政專員</div>
+                    <div class="first_second_line">呂聖鈴</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line">研究計畫及財產等事務<br>shenq@mail.nctu.edu.tw<br>03-5712121 # 56902</div>
+                </div>
+            </div>
+            <div class="ra-circle_first_block_2">
+                <div class="ra-circle_2">
+                </div>
+                <div class="first_block_context_block">
+                    <div class="first_first_line">技士</div>
+                    <div class="first_second_line">郭珍佑</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line"><p>館舍及公儀維修 門禁管理</p><p>系教評會業務 排課</p><p>cynthia@mail.nctu.edu.tw</p><p>03-5712121 # 56901</p></div>
+                </div>
+            </div>
+            </div>
+            <div class="page-2">
+            <div class="ra-circle_first_block_1">
+                <div class="ra-circle_3"></div>
+                <div class="first_block_context_block">
+                    <div class="first_first_line">行政專員</div>
+                    <div class="first_second_line">郭淑卿</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line">院務及研究生相關事務<br>sunny@mail.nctu.edu.tw<br>03-5712121 # 56937</div>
+                </div>
+            </div>
+            <div class="ra-circle_first_block_2">
+                <div class="ra-circle_7">
+                </div>
+                <div class="first_block_context_block">
+                    <div class="first_first_line">行政專員</div>
+                    <div class="first_second_line">郭貞賢</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line"><p>科技部計畫、系所會務、院士事務</p><p>g0431@nctu.edu.tw</p><p>03-5712121 # 56938</p></div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="ra-circle_second">
+            <div class="page-1">
+            <div class="ra-circle_second_block_1">
+                <div class="ra-circle_4">
+                </div>
+                <div class="first_block_context_block">
+                    <div class="first_first_line">行政專員</div>
+                    <div class="first_second_line">吳佳文</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line">專題演講 實驗室環安<br>chiawen@mail.nctu.edu.tw<br>03-5712121 # 56943</div>
+                </div>
+            </div>
+            <div class="ra-circle_second_block_2">
+                <div class="ra-circle_5"></div>
+                <div class="second_block_context_block">
+                    <div class="first_first_line">行政專員</div>
+                    <div class="first_second_line">陳珮筠</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line">大學部及國際處師生<br>教學空間設備相關事務<br>g0630@nctu.edu.tw<br>03-5712121 # 56930</div>
+                </div>
+            </div>
+            </div>
+            <div class="page-2">
+            <div class="prepage"></div>
+            <div class="ra-circle_second_block_1">
+                <div class="ra-circle_6"></div>
+                <div class="first_block_context_block">
+                    <div class="first_first_line">工友</div>
+                    <div class="first_second_line">葉連發</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line">館舍維護 收發信件<br>yah@mail.nctu.edu.tw<br>03-5712121 # 53008</div>
+                </div>
+            </div>
+            <div class="ra-circle_second_block_2">
+                <div class="ra-circle_8"></div>
+                <div class="second_block_context_block">
+                    <div class="first_first_line">行政專員</div>
+                    <div class="first_second_line">賴美伶</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line">國際處事務<br>教學空間設備相關事務<br>meiling@mail.nctu.edu.tw<br>03-5712121 # 56983</div>
+                </div>
+            </div>
+            </div>
+        </div>
+                
+    </div>
+                <a class="icon_down" href="<?php echo site_url(); ?>/dean" style="opacity:1; transform: matrix(1,0,0,1,0,0);"><div class="right_icon"></div></a>
+<!--
+    <div id="icon_down_wrapper" class="icon_down_slide"></div>
+    <a href="<?php echo site_url(); ?>/dean" style="opacity:1; transform: matrix(1,0,0,1,0,0);">
+        <span onclick="iconDown()" class="icon_down"></span>
+    </a>-->
+
+</div>
+<!-- mobile down here -->
+<div class="mobile">
+    <div class="intro-bigTitle_1">聯絡我們</div>
+
+    <div class="page p0">
+        <div class="card">
+            <div class="info">
+                <div class="info-title">行政專員</div>
+                <div class="name">呂聖鈴</div>
+                <div class="line"></div>
+                <div class="information">研究計畫及財產等事務<br>shenq@mail.nctu.edu.tw<br>03-5712121 # 56902</div>
+            </div>
+            <div class="photo">
+                <div class="photo-1"></div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="photo">
+                <div class="photo-2"></div>
+            </div>
+            <div class="info">
+                <div class="info-title">技士</div>
+                <div class="name">郭珍佑</div>
+                <div class="line"></div>
+                <div class="information"><p>館舍及公儀維修 門禁管理</p><p>系教評會業務 排課</p><p>cynthia@mail.nctu.edu.tw</p><p>03-5712121 # 56901</p></div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="info">
+                <div class="info-title">行政專員</div>
+                <div class="name">郭淑卿</div>
+                <div class="line"></div>
+                <div class="information">院務及研究生相關事務<br>sunny@mail.nctu.edu.tw<br>03-5712121 # 56937</div>
+            </div>
+            <div class="photo">
+                <div class="photo-3"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="page p1">
+        <div class="card">
+            <div class="info">
+                <div class="info-title">行政專員</div>
+                <div class="name">郭貞賢</div>
+                <div class="line"></div>
+                <div class="information"><p>科技部計畫、系所會務、院士事務</p><p>g0431@nctu.edu.tw</p><p>03-5712121 # 56938</p></div>
+            </div>
+            <div class="photo">
+                <div class="photo-4"></div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="photo">
+                <div class="photo-5"></div>
+            </div>
+            <div class="info">
+                <div class="info-title">行政專員</div>
+                <div class="name">吳佳文</div>
+                <div class="line"></div>
+                <div class="information">專題演講 實驗室環安<br>chiawen@mail.nctu.edu.tw<br>03-5712121 # 56943</div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="info">
+                <div class="info-title">行政專員</div>
+                <div class="name">陳珮筠</div>
+                <div class="line"></div>
+                <div class="information">大學部及國際處師生<br>教學空間設備相關事務<br>g0630@nctu.edu.tw<br>03-5712121 # 56930</div>
+            </div>
+            <div class="photo">
+                <div class="photo-6"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="page p2">
+        <div class="card">
+            <div class="info">
+                <div class="info-title">工友</div>
+                <div class="name">葉連發</div>
+                <div class="line"></div>
+                <div class="information">館舍維護 收發信件<br>yah@mail.nctu.edu.tw<br>03-5712121 # 53008</div>
+            </div>
+            <div class="photo">
+                <div class="photo-7"></div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="photo">
+                <div class="photo-8"></div>
+            </div>
+            <div class="info">
+                <div class="info-title">行政專員</div>
+                <div class="name">賴美伶</div>
+                <div class="line"></div>
+                <div class="information">國際處事務<br>教學空間設備相關事務<br>meiling@mail.nctu.edu.tw<br>03-5712121 # 56983</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="option">
+         <div class="option-left"></div>
+         <div class="space"></div>
+         <div class="option-right"></div>
+     </div>
+</div>
+
+<script>
+    var now = 0;
+
+    $(document).ready(function(){
+        $(".page-2").hide();
+        $(".nextpage").click(function(){
+            $(".page-1").hide();
+            $(".page-2").fadeIn(1000,"swing");
+        });
+        $(".prepage").click(function(){
+            $(".page-1").fadeIn(1000,"swing");
+            $(".page-2").hide();
+        });
+
+        $('.page').hide();
+        $('.p'+now).fadeIn(1000,"swing");
+        $('.option-left').click(function(){
+            now = (now+3-1)%3;
+            $('.page').hide();
+            $('.p'+now).fadeIn(1000,"swing");
+        });
+        $('.option-right').click(function(){
+            now = (now+1)%3;
+            $('.page').hide();
+            $('.p'+now).fadeIn(1000,"swing");
+        })
+    });
+    //just querying the DOM...like a boss!
+
+/*Annie added for mobile*/
+// add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
+// function enqueue_child_theme_styles() {
+// wp_enqueue_style( 'parent-style',get_template_directory_uri().'/style.css' );
+// wp_enqueue_style( 'child-style', get_stylesheet_uri(), NULL,
+// filemtime( get_stylesheet_directory() . '/style.css' ) );
+// }
+    function iconDown(){
+        var nextpageDuration = 140;
+    document.getElementById("icon_down_wrapper").style.height = "100vh";
+        setTimeout(function(){
+        window.location = '<?php echo site_url(); ?>/dean';
+        },nextpageDuration);
+    }
+
+/*Annie added for mobile*/
+
+    var wrapper = document.querySelector("#intro-wrapper");
+    var next_buttons = document.querySelectorAll(".intro-next");
+    var prev_buttons = document.querySelectorAll(".intro-prev");
+
+    // the activeLink provides a pointer to the currently displayed item
+    var activeLink = 0;
+     
+    // setup the event listener for the next_buttons
+    for (var i = 0; i < next_buttons.length; i++) {
+        next_buttons[i].addEventListener('click', setClickedItem_1, false);
+    }
+    for (var i = 0; i < prev_buttons.length; i++) {
+        prev_buttons[i].addEventListener('click', setClickedItem_2, false);
+    }
+
+    function setClickedItem_1(e) {
+        activeLink = (activeLink + 1) % 4;
+     
+        var position = (-70)*activeLink;
+        var translateValue = "translate3d(" + position + "vw, 0px, 0)";
+        window.setTimeout(function(){wrapper.style["opacity"]="1";}, 600);
+        wrapper.style["opacity"]="0";
+        wrapper.style[transformProperty] = translateValue;
+    }
+     
+    function setClickedItem_2(e) {
+        activeLink = (activeLink + 3) % 4;
+     
+        var position = (-70)*activeLink;
+        var translateValue = "translate3d(" + position + "vw, 0px, 0)";
+        window.setTimeout(function(){wrapper.style["opacity"]="1";}, 600);
+        wrapper.style["opacity"]="0";
+        wrapper.style[transformProperty] = translateValue;
+    }
+     
+    // Dealing with Transforms
+    var transforms = ["transform",
+                "msTransform",
+                "webkitTransform",
+                "mozTransform",
+                "oTransform"];
+     
+    var transformProperty = getSupportedPropertyName(transforms);
+     
+    // vendor prefix management
+    function getSupportedPropertyName(properties) {
+        for (var i = 0; i < properties.length; i++) {
+            if (typeof document.body.style[properties[i]] != "undefined") {
+                return properties[i];
+            }
+        }
+        return null;
+    }
+
+    $(document).ready(function() {
+        $(".nav_menu").on("click",function(){});
+    });
+</script>
+<!--
+<?php wp_nav_menu (array('theme_location' => 'Menu2','container_class' => 'nav_menu','container_id' => 'nav_menu_1')); ?>
+<?php //get_sidebar(); ?>
+-->
+<?php get_footer(); ?>
---
+++ page-templates-en/template-en-cooperation.php
@@ -0,0 +1,255 @@
+<?php
+/*
+ * Template Name: en-cooperation
+ */
+?>
+
+<?php get_header('en_header'); ?>
+
+<div class="page-intro">
+    <!--<div class="container intro-mobile-container">
+        <div id="intro-container">
+            <div id="intro-wrapper">-->
+                <div class="intro-subpage-1">    <!--first page-->
+                    <div class="intro-bigTitle_1">產學合作 英文版</div>
+                    <!--下半部分-->
+                    <div class="ra-circle_block">
+                        <div class="ra-circle_first">
+                            <div class="ra-circle_l">
+                                <div class="circle_p1_1 textbox textbox0">
+                                    台達電子工業
+                                </div>
+                                <div class="circle_p1_1 textbox textbox1">
+                                    測試第二第一
+                                </div>
+                                <div class="circle_p1_1 textbox textbox2">
+                                    測試第三第一
+                                </div>
+                                <div class="circle_p2 textbox textbox0">
+                                    股份有限公司       
+                                </div>
+                                <div class="circle_p2 textbox textbox1">
+                                    股份有限公司       
+                                </div>
+                                <div class="circle_p2 textbox textbox2">
+                                    股份有限公司       
+                                </div>
+                            </div>
+                            <div class="ra-circle_2">
+                                <div class="circle_p1_1 textbox textbox0">
+                                    宣捷生物科技
+                                </div>
+                                <div class="circle_p1_2 textbox textbox1">
+                                    四字測試
+                                </div>
+                                <div class="circle_p1_1 textbox textbox2">
+                                    炯毅我做好了
+                                </div>
+                                <div class="circle_p2 textbox textbox0">
+                                    股份有限公司
+                                </div>
+                                <div class="circle_p2 textbox textbox1">
+                                    股份有限公司
+                                </div>
+                                <div class="circle_p2 textbox textbox2">
+                                    股份有限公司
+                                </div>
+                            </div>
+                            <div class="ra-circle_3">
+                                <div class="circle_p1_1 textbox textbox0">
+                                    康健基因科技
+                                </div>
+                                <div class="circle_p1_1 textbox textbox1">
+                                    康健基因科技
+                                </div>
+                                <div class="circle_p1_1 textbox textbox2">
+                                    康健基因科技
+                                </div>
+                                <div class="circle_p2 textbox textbox0">
+                                    股份有限公司
+                                </div>
+                                <div class="circle_p2 textbox textbox1">
+                                    股份有限公司
+                                </div>
+                                <div class="circle_p2 textbox textbox2">
+                                    股份有限公司
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ra-circle_second">
+                            <div class="ra-circle_4">
+                                <div class="circle_p1_1 textbox textbox0">
+                                    康鶴生醫科技
+                                </div>
+                                <div class="circle_p1_1 textbox textbox1">
+                                    康鶴生醫科技
+                                </div>
+                                <div class="circle_p1_1 textbox textbox2">
+                                    康鶴生醫科技
+                                </div>
+                                <div class="circle_p2 textbox textbox0">
+                                    股份有限公司
+                                </div>
+                                <div class="circle_p2 textbox textbox1">
+                                    股份有限公司
+                                </div>
+                                <div class="circle_p2 textbox textbox2">
+                                    股份有限公司
+                                </div>
+                            </div>
+                            <div class="ra-circle_5">
+                                <div class="circle_p1_2 textbox textbox0">
+                                    琦芯科技
+                                </div>
+                                <div class="circle_p1_2 textbox textbox1">
+                                    琦芯科技
+                                </div>
+                                <div class="circle_p1_2 textbox textbox2">
+                                    琦芯科技
+                                </div>
+                                <div class="circle_p2 textbox textbox0">
+                                    股份有限公司
+                                </div>
+                                <div class="circle_p2 textbox textbox1">
+                                    股份有限公司
+                                </div>
+                                <div class="circle_p2 textbox textbox2">
+                                    股份有限公司
+                                </div>
+                            </div>
+                            <div class="ra-circle_6">
+                                <div class="circle_p1_2 textbox textbox0">
+                                    瀚源生醫
+                                </div>
+                                <div class="circle_p1_2 textbox textbox1">
+                                    瀚源生醫
+                                </div>
+                                <div class="circle_p1_2 textbox textbox2">
+                                    瀚源生醫
+                                </div>
+                                <div class="circle_p2 textbox textbox0">
+                                    股份有限公司
+                                </div>
+                                <div class="circle_p2 textbox textbox1">
+                                    股份有限公司
+                                </div>
+                                <div class="circle_p2 textbox textbox2">
+                                    股份有限公司
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <!--</div>
+        </div>
+    </div>-->
+
+<!--往左右頁移動-->
+    <!--<div class="left"></div>-->
+    <!--<div class="right"></div>-->
+
+                <!--<a class="icon_down" href="<?php echo site_url(); ?>/dean" style="opacity:1; transform: matrix(1,0,0,1,0,0);"><div class="right_icon"></div></a>
+
+    <div id="icon_down_wrapper" class="icon_down_slide"></div>
+    <a href="<?php echo site_url(); ?>/dean" style="opacity:1; transform: matrix(1,0,0,1,0,0);">
+        <span onclick="iconDown()" class="icon_down"></span>
+    </a>-->
+
+</div>
+
+
+<script>
+    //just querying the DOM...like a boss!
+
+/*Annie added for mobile*/
+// add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
+// function enqueue_child_theme_styles() {
+// wp_enqueue_style( 'parent-style',get_template_directory_uri().'/style.css' );
+// wp_enqueue_style( 'child-style', get_stylesheet_uri(), NULL,
+// filemtime( get_stylesheet_directory() . '/style.css' ) );
+// }
+    function iconDown(){
+        var nextpageDuration = 140;
+    document.getElementById("icon_down_wrapper").style.height = "100vh";
+        setTimeout(function(){
+        window.location = '<?php echo site_url(); ?>/dean';
+        },nextpageDuration);
+    }
+
+/*Annie added for mobile*/
+
+    var wrapper = document.querySelector("#intro-wrapper");
+    var next_buttons = document.querySelectorAll(".intro-next");
+    var prev_buttons = document.querySelectorAll(".intro-prev");
+
+    // the activeLink provides a pointer to the currently displayed item
+    var activeLink = 0;
+     
+    // setup the event listener for the next_buttons
+    for (var i = 0; i < next_buttons.length; i++) {
+        next_buttons[i].addEventListener('click', setClickedItem_1, false);
+    }
+    for (var i = 0; i < prev_buttons.length; i++) {
+        prev_buttons[i].addEventListener('click', setClickedItem_2, false);
+    }
+
+    function setClickedItem_1(e) {
+        activeLink = (activeLink + 1) % 4;
+     
+        var position = (-70)*activeLink;
+        var translateValue = "translate3d(" + position + "vw, 0px, 0)";
+        window.setTimeout(function(){wrapper.style["opacity"]="1";}, 600);
+        wrapper.style["opacity"]="0";
+        wrapper.style[transformProperty] = translateValue;
+    }
+     
+    function setClickedItem_2(e) {
+        activeLink = (activeLink + 3) % 4;
+     
+        var position = (-70)*activeLink;
+        var translateValue = "translate3d(" + position + "vw, 0px, 0)";
+        window.setTimeout(function(){wrapper.style["opacity"]="1";}, 600);
+        wrapper.style["opacity"]="0";
+        wrapper.style[transformProperty] = translateValue;
+    }
+     
+    // Dealing with Transforms
+    var transforms = ["transform",
+                "msTransform",
+                "webkitTransform",
+                "mozTransform",
+                "oTransform"];
+     
+    var transformProperty = getSupportedPropertyName(transforms);
+     
+    // vendor prefix management
+    function getSupportedPropertyName(properties) {
+        for (var i = 0; i < properties.length; i++) {
+            if (typeof document.body.style[properties[i]] != "undefined") {
+                return properties[i];
+            }
+        }
+        return null;
+    }
+
+    var now = 0;
+
+    $(document).ready(function() {
+        $(".nav_menu").on("click",function(){});
+        $('.textbox').hide();
+        $('.textbox'+now).fadeIn(1000,"swing");
+        $('.left').click(function(){
+            now = (now+3-1)%3;
+            $('.textbox').hide();
+            $('.textbox'+now).fadeIn(1000,"swing");
+        });
+        $('.right').click(function(){
+            now = (now+1)%3;
+            $('.textbox').hide();
+            $('.textbox'+now).fadeIn(1000,"swing");
+        })
+    });
+</script>
+<!--<?php wp_nav_menu (array('theme_location' => 'Menu2','container_class' => 'nav_menu','container_id' => 'nav_menu_1')); ?>
+<?php //get_sidebar(); ?>-->
+<?php get_footer(); ?>
